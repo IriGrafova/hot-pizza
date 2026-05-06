84592bb135aa76cfcf9c969b372ba3b039d32438
i.grafova <i.grafova@ntc-hitech.ru>
2026-05-06 18:05:49 +0300

    feat: add complete pizza delivery app

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
+import { MainPageComponent } from "./pages/main-page/main-page.component";
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [],
+  imports: [MainPageComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
---
+++ src/app/mocks/products.mock.ts
@@ -0,0 +1,52 @@
+import { Product } from '../models/product.interface';
+
+export const PRODUCTS_MOCK: Product[] = [
+  {
+    id: 1,
+    name: 'Мясная Делюкс',
+    description: 'Пепперони, лук, бекон, томатная паста, колбаски, перец, грибы, соус чили, ананасы',
+    image: 'assets/images/pizza1.png'
+  },
+  {
+    id: 2,
+    name: 'Морская Премиум',
+    description: 'Перец, сыр, креветки, кальмары, мидии, лосось',
+    image: 'assets/images/pizza2.png'
+  },
+  {
+    id: 3,
+    name: 'Бекон и Сосиски',
+    description: 'Бекон, сыр, сосиски, ананас, томатная паста',
+    image: 'assets/images/pizza3.png'
+  },
+  {
+    id: 4,
+    name: 'Куриная Делюкс',
+    description: 'Курица, ананас, сыр Пепперони, соус для пиццы, томатная паста',
+    image: 'assets/images/pizza4.png'
+  },
+  {
+    id: 5,
+    name: 'Барбекю Премиум',
+    description: 'Свинина BBQ, соус Барбкею, сыр, курица, соус для пиццы, соус чили',
+    image: 'assets/images/pizza5.png'
+  },
+  {
+    id: 6,
+    name: 'Пепперони Дабл',
+    description: 'Пепперони, сыр, колбаса 2 видов: обжаренная и вареная',
+    image: 'assets/images/pizza6.png'
+  },
+  {
+    id: 7,
+    name: 'Куриное трио',
+    description: 'Жареная курица, Тушеная курица, Куриные наггетсы, перец, сыр, грибы, соус для пиццы',
+    image: 'assets/images/pizza7.png'
+  },
+  {
+    id: 8,
+    name: 'Сырная',
+    description: 'Сыр Джюгас, Сыр с плесенью, Сыр Моцарелла, Сыр секретный',
+    image: 'assets/images/pizza8.png'
+  }
+];
